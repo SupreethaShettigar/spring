4b68aa81744e499695e86141867a11f224e92421
SupreethaShettigar <56212148+SupreethaShettigar@users.noreply.github.com>
2019-11-05 16:39:00 +0530

    Add files via upload

---
+++ spring-core/spring/src/main/java/com/HelloWorld.java
@@ -0,0 +1,16 @@
+package com;
+
+public class HelloWorld {
+	String msg;
+
+	public HelloWorld() {
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
